5fcfa990e7dda099567226e0d43bf9fddd1a73ab
Jessica Morrison <jnicomorr@gmail.com>
2022-09-10 11:54:37 -0500

    Fixed edit ui

--- components/forms/commentForm.js
+++ components/forms/commentForm.js
@@ -66,10 +66,10 @@ export default function CommentForm({ obj }) {
     <Form onSubmit={handleSubmit}>
       <h2 style={{ color: '#84190B', font: 'bold', 'font-size': '5rem' }} className="mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Comment</h2>
       <FloatingLabel controlId="floatingInput1" label="Title" className="mb-3">
-        <Form.Control type="text" placeholder="Title" name="commentTitle" value={formInput.commentTitle} onChange={handleChange} required />
+        <Form.Control style={{ padding: '4rem' }} type="text" placeholder="Title" name="commentTitle" value={formInput.commentTitle} onChange={handleChange} required />
       </FloatingLabel>
       <FloatingLabel controlId="floatingInput3" label="Say what now?" className="mb-3">
-        <Form.Control type="text" placeholder="Comment" name="comment" value={formInput.comment} onChange={handleChange} required />
+        <Form.Control style={{ padding: '4rem' }} type="text" placeholder="Comment" name="comment" value={formInput.comment} onChange={handleChange} required />
       </FloatingLabel>
       <FloatingLabel controlId="floatingSelect">
         <Form.Select aria-label="movie" name="movieFirebaseKey" onChange={handleChange} className="mb-3" value={movies.firebaseKey} required>
