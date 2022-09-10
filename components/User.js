89a551350b96ffa282805b7226183f490d7b7dac
Jessica Morrison <jnicomorr@gmail.com>
2022-09-10 16:57:37 -0500

    Made changes to UserCard

--- components/User.js
+++ components/User.js
@@ -1,33 +1,31 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
+import { useAuth } from '../utils/context/authContext';
 
 // *Stretch* add tag line and about me
 
-export default function User({
-  name, image,
-}) {
+export default function User() {
+  const { user } = useAuth();
+
   return (
-    <Card className="text-center" style={{ width: '25rem', height: '40rem' }}>
+    <Card className="userCard" style={{ width: '25rem', height: '40rem' }}>
       <Card.Header />
       <Card.Body>
-        <Card.Img src={image} alt={name} style={{ borderRadius: '70%', width: '70%' }} />
-        <Card.Body style={{ marginTop: '7rem' }}>
-          <Card.Title>{name}</Card.Title>
+        <Card.Img className="userImage" src={user.photoURL} alt={user.displayName} style={{ borderRadius: '70%', width: '70%' }} />
+        <Card.Body className="userName" style={{ marginTop: '7rem' }}>
+          <Card.Title>{user.displayName}</Card.Title>
         </Card.Body>
       </Card.Body>
-      <Card.Footer className="text-muted">Last Login: {Date().toLocaleString()}</Card.Footer>
+      <Card.Footer style={{ marginTop: '25rem' }} className="text-muted">Last Login: {Date().toLocaleString()}</Card.Footer>
     </Card>
   );
 }
 
-User.propTypes = {
-  name: PropTypes.string,
-  image: PropTypes.string,
-};
-
-User.defaultProps = {
-  name: 'Jess',
-  image: 'https://image.shutterstock.com/image-photo/red-fox-animal-600w-789993919.jpg',
-};
+// User.propTypes = {
+//   userObj: PropTypes.shape({
+//     name: PropTypes.string,
+//     image: PropTypes.string,
+//   }).isRequired,
+// };
