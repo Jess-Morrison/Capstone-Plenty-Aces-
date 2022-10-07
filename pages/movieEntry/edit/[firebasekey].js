a4e1e06bbeb2bb8a0a66d6d64a6e5e2b07c57c88
Jessica Morrison <jnicomorr@gmail.com>
2022-10-07 18:31:46 -0500

    try 2

--- pages/movieEntry/edit/[firebasekey].js
+++ pages/movieEntry/edit/[firebasekey].js
@@ -8,7 +8,6 @@ export default function EditMovieEntry() {
   const router = useRouter();
   const { firebaseKey } = router.query;
 
-  // Adding to fix routing error
   useEffect(() => {
     getSingleMovie(firebaseKey).then(setEditMovieItem);
   }, [firebaseKey]);
